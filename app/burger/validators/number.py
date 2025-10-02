43ccb05ca385a4a86fc13e662fd6b31dcc1efae9
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2025-10-02 12:09:52 +0200

    implemnting Number classe

---
+++ app/burger/validators/number.py
@@ -0,0 +1,14 @@
+from app.burger.validators.validator import Validator
+
+
+class Number(Validator):
+    def __init__(self, min_value: int, max_value: int):
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def validate(self, value):
+        if not isinstance(value, int):
+            raise TypeError('Quantity should be integer.')
+        if value < self.min_value or value > self.max_value:
+            raise ValueError("Quantity should not be less than {self.min_value} and greater than {self.max_value}.")
+        return value
